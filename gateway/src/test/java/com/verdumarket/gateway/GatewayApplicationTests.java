5a3406bd70acf72fa5949930cd2edcbb74161c35
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-02-16 15:16:52 -0300

    Agregando archivos iniciales al repositorio

---
+++ gateway/src/test/java/com/verdumarket/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdumarket.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
