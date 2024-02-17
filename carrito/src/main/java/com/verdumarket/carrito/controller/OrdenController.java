55db2927cf71fe944238db8fcc41d3b1a7c23fed
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-02-17 16:30:35 -0300

    alguna cosa de controlador

--- carrito/src/main/java/com/verdumarket/carrito/controller/OrdenController.java
+++ carrito/src/main/java/com/verdumarket/carrito/controller/OrdenController.java
@@ -1,7 +1,17 @@
 package com.verdumarket.carrito.controller;
 
+import com.verdumarket.carrito.model.Orden;
+import com.verdumarket.carrito.service.IOrdenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrdenController {
+    @Autowired
+    private IOrdenService ordenService;
+
+    @GetMapping("/")
+    public ResponseEntity listarOrdenesPorConsumidor()
 }
